50f19b05964a557647a85fc0832a40d4e420f1a6
brunoluizgn <brunoluiz.gn@gmail.com>
2023-05-11 16:06:45 -0300

    atual

--- interpolacao.py
+++ interpolacao.py
@@ -1,3 +1,11 @@
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal 
+
+"""
 nome= 'luiz'
 preco= 1000.955548
 variavel = '%s, o preço e %f' % (nome, preco)

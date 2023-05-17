58382721982cb497117a76a429c59308275700a3
brunoluizgn <brunoluiz.gn@gmail.com>
2023-05-17 16:27:44 -0300

    correção exercícios

--- exercicio_hora.py
+++ exercicio_hora.py
@@ -3,13 +3,12 @@ descrito, exiba a saudação apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 entrada = input ('Digite a hora em números inteiros: ')
-
 try:
     hora = int(entrada)
     
-    if hora >= 0 and hora <=11:
+    if hora >= 0 and hora <= 11:
         print('Bom dia')
-    elif hora >= 12 and hora <=17:
+    elif hora >= 12 and hora <= 17:
         print('Boa tarde')    
     elif hora >= 18 and hora <= 23:
         print(' Boa noite!')
@@ -17,4 +16,4 @@ try:
         print(' Não conhce essa hora')
 except:
     print('por favor, digite apenas números inteiros')
-    
+
